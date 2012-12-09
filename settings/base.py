76f90576ea7ea40d2637905f3853d736a67829b9
ossreleasefeed <sneethling@mozilla.com>
2012-12-09 18:55:46 +0200

    initial version of plugincheck for review

--- settings/base.py
+++ settings/base.py
@@ -218,6 +218,10 @@ MINIFY_BUNDLES = {
         'persona': (
             'css/persona.less',
         ),
+        'plugincheck': (
+            'css/plugincheck/plugincheck.less',
+            'css/plugincheck/qtip.css',
+        ),
         'privacy': (
             'css/privacy.less',
         ),
@@ -371,6 +375,12 @@ MINIFY_BUNDLES = {
             'js/libs/jquery.validate.js',
             'js/partnerships.js',
             'js/mozilla-input-placeholder.js',
+        ),
+        'plugincheck': (
+            'js/plugincheck/plugincheck.js',
+            'js/plugincheck/lib/mustache.js',
+            'js/plugincheck/tmpl/plugincheck.ui.tmpl.js',
+            'js/plugincheck/check-plugins.js',
         ),
         'privacy': (
             'js/mozilla-pager.js',
